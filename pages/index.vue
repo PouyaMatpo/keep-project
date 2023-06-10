9406c385d7f0bac0df9a0c44cf77ad73345569a2
pouyamatpo <pouya.mirnia@gmail.com>
2023-06-10 10:51:33 +0330

    s

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
     <template >
-    <v-container fluid class="pa-0 ma-0">
+    <v-container fluid class="pa-0 ma-0">sssss
         <add-bar />
         <!-- <v-card max-width="100%" flat >
          <v-list>
